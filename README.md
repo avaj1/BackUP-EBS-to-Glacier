f22815dab24ad0cc0134fda832f73e718a1c461b
Aniruddha <legacy@localhost.localdomain>
2015-07-27 16:38:00 +0530

    adding logger, mountpoint check

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 
 ### BackUP-EBS-to-Glacier
 
-These script is created with a purpose to backup(tar) all the EBS volumes attached to EC2 instances which are in **stopped** state and upload the tarball to AWS Glacier, script will delete the volumes attached to instances once backup is done. Script should be running on completely new instance, which will also generate CSV report file containg data such as Instane-Id, Volume-Id, Archive-Id on which the operations have been performed.
+These script is created with a purpose to backup(tar) all the EBS volumes attached to EC2 instances which are in **stopped** state and upload the tarball to AWS Glacier, script will delete the volumes attached to instances once backup is done. Script should be running on completely new instance, which will also generate CSV report file containg data such as Instane-Id, Volume-Id, Archive-Id on which the operations have been performed. A logger file will also be generated to go through failure logs.
 
 Pre-requisites:
-EC2 instance - For running these script
+EC2 instance with boto installed - For running these script
 
 ##### Environment Setup
 1. Launch a new EC2 instance
@@ -14,8 +14,8 @@ EC2 instance - For running these script
 ##### How it Works:
 1. Script will pick one of the instances in stopped state and detach the volume attached to it. These volume is then attched to new EC2 instance (one where these script is running)
 2. Attached volume will be mounted and tar file of the entire volume will be created
-3. These tar file will be uploaded to AWS Glacier which in turn will reterive the archive-id
-4. The volume attached will now be un-mounted and deleted
+3. These tarball file will be uploaded to AWS Glacier which in turn will reterive the archive-id (Copy the archive-id to download the uploaded file)
+4. The volume attached will now be un-mounted and re-attached to previous machines
 
 ##### Running the Script
 $ python movevoltogal.py
--- movevoltogal.py
+++ movevoltogal.py
@@ -7,6 +7,18 @@ import time
 import subprocess
 import json
 import csv
+import logging
+
+
+def mountpointexists():
+    if os.path.exists("/dev/xvdf1"):
+        subprocess.call(
+            "sudo mount /dev/xvdf1 ~/vol/",
+            shell=True)
+    else:
+        subprocess.call(
+            "sudo mount /dev/xvdf ~/vol/",
+            shell=True)
 
 if os.path.isfile("config.json"):
     with open("config.json", 'r') as cfg_data:
@@ -17,18 +29,28 @@ if os.path.isfile("config.json"):
 else:
     print "config.json does not exist " + e
 
-fieldnames = ['Instance_ID', 'Volume_ID', 'Block_Device_Name', 'Achive_ID']
+logger = logging.getLogger("logger_movevoltogal")
+log_format = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(
+    filename=data['log_filename'],
+    format=log_format,
+    datefmt='%d/%m/%Y %H:%M:%S',
+    level=logging.INFO)
+
+fieldnames = ['Instance_ID', 'Volume_ID',
+              'Block_Device_Name', 'Vault_Name', 'Archive_ID']
 
 conn = boto.ec2.connect_to_region(
     data['region'],
     aws_access_key_id=data['aws_access_key_id'],
     aws_secret_access_key=data['aws_secret_access_key'])
 if conn is None:
-    print "Error"
+    logger.error("Error connecting AWS")
 else:
     reservations = conn.get_all_reservations(
         filters={
-            "instance-state-name": "stopped"})
+            "instance-state-name": data['instance_state'],
+            "availability_zone": data['availability-zone']})
     for reservation in reservations:
         try:
             for instance in reservation.instances:
@@ -38,78 +60,96 @@ else:
                 bdm = instance.block_device_mapping
                 try:
                     for device in bdm:
-                        bdt = bdm[device]    		# bdt=block-device-type
+                        bdt = bdm[device]        	# bdt=block-device-type
                         volid = bdt.volume_id
-                        print "--"+instanceid+"-----"+device+"-----"+volid+"--"
+                        print "\n--" + instanceid + "-----" + device + "-----" + volid + "--"
+                        logger.info("--" + instanceid + "-----" +
+                                    device + "-----" + volid + "--")
 
         # Detach volume and attach to temporary instance
                         try:
                             conn.detach_volume(volid)
                             time.sleep(10)
-                            conn.attach_volume(
-                                volid,
-                                data['instance_id'],
-                                "/dev/sdf")
-                            time.sleep(10)                            
+                            vol = conn.get_all_volumes(volid)
+                            volstatus = vol[0].attachment_state()
+                            if volstatus is None:
+                                logger.info(
+                                    volid + " is detach from " + instanceid)
+                                check = conn.attach_volume(
+                                    volid,
+                                    data['instance_id'],
+                                    "/dev/sdf")
+                                time.sleep(10)
+                                check1 = conn.get_all_volumes(volid)
+                                checks = check1[0].attachment_state()
+                                logger.info(
+                                    volid + " is attached to " +
+                                            data['instance_id'])
                         except Exception as e:
-                            print "\n Error in attaching volume: " + str(e)
+                            logger.error(
+                                "Volume seems busy, can't attach: " + str(e))
 
         # Perform a archive operation of attched volume
+                        archive_file = tagvalue + "_" + instanceid + "_" + volid
                         try:
-                            if os.path.isdir("/home/ec2-user/vol/"):
+                            voldir = os.path.isdir("/home/ec2-user/vol/")
+                            if voldir is False:
                                 subprocess.call(
-                                    "sudo mount /dev/xvdf1 ~/vol/",
+                                    "sudo mkdir ~/vol/ ",
                                     shell=True)
+                                mountpointexists()
                             else:
-                                subprocess.call(
-                                    "sudo mkdir ~/vol/",
-                                    shell=True)
-                            subprocess.call(
-                                "sudo mount /dev/xvdf1 ~/vol/",
-                                shell=True)
+                                mountpointexists()
                             subprocess.call(
                                 "sudo tar -czf " +
-                                volid +
+                                archive_file +
                                 ".tar.gz ~/vol/* &> /dev/null",
                                 shell=True)
                             print "Archive done for " + volid
                             subprocess.call("sudo umount ~/vol/", shell=True)
                         except Exception as e:
-                            print "Error while archive operation : " + str(e)
+                            logger.error("Error in archive: " + str(e))
 
         # Upload archive file to AWS Glacier
                         try:
                             vault_name = str(data['vault_name'])
                             part_size = int(data['part_size'])
                             num_threads = int(data['num_threads'])
-                            print "Backing Up archive of "+volid+" to Glacier"
-                            galcierconn = Layer1(
+                            print "Backing archive of " + volid + " to Glacier"
+                            glacierconn = Layer1(
                                 aws_access_key_id=data['aws_access_key_id'],
                                 aws_secret_access_key=data[
                                     'aws_secret_access_key'],
                                 region_name=data['region'])
-                            vault = galcierconn.describe_vault(vault_name)
+                            vault = glacierconn.describe_vault(vault_name)
                             uploader = ConcurrentUploader(
-                                galcierconn,
+                                glacierconn,
                                 vault_name,
                                 part_size=part_size,
                                 num_threads=num_threads)
                             archive_id = uploader.upload(
-                                (volid + ".tar.gz"),
+                                (archive_file + ".tar.gz"),
                                 "first upload")
                             print "Download the archive using: " + archive_id
                         except Exception as e:
-                            print "Glacier error: " + str(e)
+                            logger.error("Glacier error: " + str(e))
 
         # Detach the volume from temporary instance and associate it back to
         # respective instance
                         try:
                             conn.detach_volume(volid)
-                            time.sleep(10)
-                            conn.delete_volume(volid)
-                            time.sleep(10)
+                            time.sleep(15)
+                            vol = conn.get_all_volumes(volid)
+                            volstatus = vol[0].attachment_state()
+                            if volstatus is None:
+                                logger.info(
+                                    volid + " is detach from " +
+                                    data['instance_id'])
+                                conn.attach_volume(volid, instanceid, device)
+                                logger.info(volid + " deleted")
                         except Exception as e:
-                            print "Error: " + str(e)
+                            logger.error(
+                                "Volume seems busy, can't delete: " + str(e))
 
         # Generate CSV file
                         if os.path.isfile(data['csv_filename']):
@@ -135,7 +175,7 @@ else:
                         reporter.writerow(csv_values)
 
                 except Exception as e:
-                    print str(e)
+                    logger.error(str(e))
                     continue
         except Exception as e:
-            print str(e)
+            logger.error(str(e))
